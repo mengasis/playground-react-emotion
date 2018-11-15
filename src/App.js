bdc610dc0730e6281030fc563da5ae84e595081b
Manuel Venegas <mengasis@outlook.com>
2018-11-15 17:21:08 -0300

    Replacement of CSS by CSS in js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+
+import styled, { keyframes } from 'react-emotion'
+
+const Header = styled('header')`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`
+
+const spin = keyframes`
+	from {
+    transform: rotate(0deg);
+	}
+  	to {
+    transform: rotate(360deg);
+	}
+`
+
+const Logo = styled('img')`
+	animation: ${spin} infinite 20s linear;
+	height: 40vmin;
+`
+
+const Link = styled('a')`
+	color: #61dafb;
+`
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Header>
+				<Logo src={logo} alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<Link
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</Link>
+			</Header>
+		)
+	}
 }
 
-export default App;
+export default App
