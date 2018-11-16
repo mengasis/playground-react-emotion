9b75158e44de642c0f3609a03a5606b1d9de85ed
Manuel Venegas <mengasis@outlook.com>
2018-11-16 01:12:32 -0300

    Added new section

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import styled from 'react-emotion'
+
+export default styled('div')`
+	display: flex;
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+	color: white;
+	background-color: #fff;
+	padding: 20px;
+`
